89d7e0efcd91a93b48fbd3655def880145d432d6
David Munoz <davidlmunoz.2024@icloud.com>
2026-04-18 21:46:06 -0500

    Update and rename gitkeep to loadComponents.js

---
+++ js/loadComponents.js
@@ -0,0 +1,13 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarContainer = document.getElementById("navbar");
+
+  if (navbarContainer) {
+    fetch("components/navbar.html")
+      .then(response => response.text())
+      .then(data => {
+        navbarContainer.innerHTML = data;
+      })
+      .catch(error => console.error("Error loading navbar:", error));
+  }
+});
